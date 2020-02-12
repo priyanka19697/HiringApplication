d0d34928eefe4baf68e115962045995a33e2cd37
priyanka19697 <priyanka.pakki@sojern.com>
2020-02-13 00:41:46 +0530

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+roles = Role.create([{name:"HIRING MANAGER"}, {name:"INTERVIEWER"}])
+jobs = Job.create([{name:"SWE"}, {name:"SSE"}, {name:"LEAD"},{name:"PARTNER"}])
